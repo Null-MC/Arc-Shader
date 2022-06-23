e386d42fa4dc4674514832a9d10101f8df3a517e
NULL511 <null511@outlook.com>
2022-06-23 03:27:24 -0400

    cleanup options

--- shaders/overworld.glsl
+++ shaders/overworld.glsl
@@ -21,3 +21,5 @@ const bool shadowHardwareFiltering1 = true;
 
 const vec4 shadowcolor0ClearColor = vec4(0.0, 0.0, 0.0, 0.0);
 const vec4 shadowcolor1ClearColor = vec4(0.0, 0.0, 0.0, 0.0);
+
+const float ambientOcclusionLevel = 0.5f;
