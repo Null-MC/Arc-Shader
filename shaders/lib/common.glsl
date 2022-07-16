e3fdc69d915e943ffe43ec48d013652f9c0680fc
NULL511 <null511@outlook.com>
2022-07-16 14:20:25 -0400

    update settings/profile defaults

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -111,7 +111,7 @@ const bool colortex9Clear = false;
 
 
 // Camera Options
-#define CAMERA_EXPOSURE_MODE 1 // [0 1 2]
+#define CAMERA_EXPOSURE_MODE 2 // [0 1 2]
 #define CAMERA_EXPOSURE 0 // [-17 -16 -15 -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6]
 #define CAMERA_LUM_MIN 20.0
 #define CAMERA_LUM_MAX 20000.0
