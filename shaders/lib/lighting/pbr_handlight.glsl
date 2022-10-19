af756f7f528be8a58ff166479595c21ccd3699bd
NULL511 <null511@outlook.com>
2022-10-19 04:10:58 -0400

    improve handlight

--- shaders/lib/lighting/pbr_handlight.glsl
+++ shaders/lib/lighting/pbr_handlight.glsl
@@ -1,9 +1,20 @@
+uniform int heldItemId;
+uniform int heldItemId2;
+
+vec3 itemLightColors[5] = vec3[](
+    vec3(1.0, 0.2, 0.0),
+    vec3(0.0, 0.3, 0.9),
+    vec3(0.8, 0.6, 0.2),
+    vec3(0.3, 0.6, 0.8),
+    vec3(0.8, 0.0, 0.7));
+
+
 float GetHandLightAttenuation(const in float lightLevel, const in float lightDist) {
-    float diffuseAtt = max(0.0625*lightLevel - 0.08*lightDist, 0.0);
-    return pow5(diffuseAtt) * 10.0;
+    float diffuseAtt = saturate(0.0625*lightLevel - 0.08*lightDist);
+    return pow(diffuseAtt, 3);
 }
 
-void _ApplyHandLighting(out vec3 diffuse, out vec3 specular, const in vec3 albedo, const in float f0, const in int hcm, const in float scattering, const in vec3 lightPos, const in int lightLevel, const in vec3 viewNormal, const in vec3 viewPos, const in vec3 viewDir, const in float NoVm, const in float roughL) {
+void _ApplyHandLighting(out vec3 diffuse, out vec3 specular, const in vec3 albedo, const in float f0, const in int hcm, const in float scattering, const in vec3 lightPos, const in int lightLevel, const in vec3 viewNormal, const in vec3 viewPos, const in vec3 viewDir, const in float NoVm, const in float roughL, const in int itemId) {
     vec3 lightDir = normalize(lightPos);
 
     float NoL = dot(viewNormal, lightDir);
@@ -20,7 +31,12 @@ void _ApplyHandLighting(out vec3 diffuse, out vec3 specular, const in vec3 albed
     vec3 halfDir = normalize(lightDir + viewDir);
     float LoHm = max(dot(lightDir, halfDir), 0.0);
 
-    vec3 handLightColor = blockLightColor * attenuation;
+    vec3 handLightColor;
+
+    if (itemId > 0 && itemId <= 5)
+        handLightColor = attenuation * itemLightColors[itemId-1] * BlockLightLux;
+    else
+        handLightColor = attenuation * blockLightColor;
 
     if (isEyeInWater == 1) {
         float viewDist = length(viewPos);
@@ -47,14 +63,14 @@ void ApplyHandLighting(out vec3 diffuse, out vec3 specular, const in vec3 albedo
     vec3 specularMain = vec3(0.0);
     if (heldBlockLightValue > 0) {
         vec3 lightPosMain = handOffsetMain - viewPos;
-        _ApplyHandLighting(diffuseMain, specularMain, albedo, f0, hcm, scattering, lightPosMain, heldBlockLightValue, viewNormal, viewPos, viewDir, NoVm, roughL);
+        _ApplyHandLighting(diffuseMain, specularMain, albedo, f0, hcm, scattering, lightPosMain, heldBlockLightValue, viewNormal, viewPos, viewDir, NoVm, roughL, heldItemId);
     }
 
     vec3 diffuseAlt = vec3(0.0);
     vec3 specularAlt = vec3(0.0);
     if (heldBlockLightValue2 > 0) {
         vec3 lightPosAlt = handOffsetAlt - viewPos;
-        _ApplyHandLighting(diffuseAlt, specularAlt, albedo, f0, hcm, scattering, lightPosAlt, heldBlockLightValue2, viewNormal, viewPos, viewDir, NoVm, roughL);
+        _ApplyHandLighting(diffuseAlt, specularAlt, albedo, f0, hcm, scattering, lightPosAlt, heldBlockLightValue2, viewNormal, viewPos, viewDir, NoVm, roughL, heldItemId2);
     }
     
     diffuse = diffuseMain + diffuseAlt;
